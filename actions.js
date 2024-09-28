68711e3312889ad71e71cb1317357d6256bff0fd
Aleksandra Pacia <olusiaop05@gmail.com>
2024-09-28 14:29:22 +0200

    check whether an input is a letter

--- actions.js
+++ actions.js
@@ -2,3 +2,6 @@ window.addEventListener('keydown', function (e) {
     document.querySelector('.letter').innerHTML = `You pressed ${e.key}`;
   }, false);
 
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
