d264139652cde3539906e49c69e24eda47d91e9e
Aleksandra Pacia <olusiaop05@gmail.com>
2024-09-28 17:44:24 +0200

    border of boxes

--- actions.js
+++ actions.js
@@ -5,3 +5,20 @@ window.addEventListener('keydown', function (e) {
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
+
+// show the loading spinner when needed
+function setLoading(isLoading) {
+    loadingDiv.classList.toggle("hidden", !isLoading);
+  }
+
+function makeMap(array) {
+    const obj = {};
+    for (let i = 0; i<array.length; i++) {
+        if (obj[array[i]]) {
+            obj[array[i]]++;
+        } else {
+            obj[array[i]] = 1;
+        }
+        return obj;
+    }
+}
