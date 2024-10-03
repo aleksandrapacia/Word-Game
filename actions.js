3173ad75435ea5339cfee5fa0a57eb33dbf6c417
Aleksandra Pacia <olusiaop05@gmail.com>
2024-10-03 18:32:28 +0200

    loading letters div

--- actions.js
+++ actions.js
@@ -1,24 +1,4 @@
-window.addEventListener('keydown', function (e) {
-    document.querySelector('.letter').innerHTML = `You pressed ${e.key}`;
-  }, false);
+const letters = document.querySelectorAll('letter');
+console.log(letters);
+const loadingDiv = document.querySelector('loading');
 
-function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
-}
-
-// show the loading spinner when needed
-function setLoading(isLoading) {
-    loadingDiv.classList.toggle("hidden", !isLoading);
-  }
-
-function makeMap(array) {
-    const obj = {};
-    for (let i = 0; i<array.length; i++) {
-        if (obj[array[i]]) {
-            obj[array[i]]++;
-        } else {
-            obj[array[i]] = 1;
-        }
-        return obj;
-    }
-}
