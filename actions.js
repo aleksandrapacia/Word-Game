a6f75599f594bde614fc244efba41e08f1694c78
Aleksandra Pacia <olusiaop05@gmail.com>
2024-10-05 14:37:03 +0200

    animation is displayed only when the page loads

--- actions.js
+++ actions.js
@@ -1,6 +1,7 @@
 const letters = document.querySelectorAll('letter');
 console.log(letters);
-//const loadingDiv = document.querySelector('loading');
+const loadingDiv = document.querySelector('#loading');
+
 
 async function init() {
 
@@ -9,5 +10,18 @@ async function init() {
 
         console.log(action)
     });
+
+    window.addEventListener('load', function() {
+        
+        this.setTimeout(function() {
+            if (loadingDiv) {
+                loadingDiv.style.display = 'none';
+            }
+        }, 1000) // 1 second
+    });
 }
+
+
 init();
+
+//TODO: loading emoji only when the page is loading
