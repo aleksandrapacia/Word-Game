5b97dfa70695a986417863196e933885ad554f7a
Aleksandra Pacia <olusiaop05@gmail.com>
2024-10-16 00:37:56 +0200

    check the position

--- actions.js
+++ actions.js
@@ -6,18 +6,38 @@ const validWords = ["code", 'game']
 const ANSWER_LENGTH = 4;
 const ROUNDS = 6;
 
-// start state
-let currentRow = 0;
-let currentGuess = '';
-let done = false;
-let isLoading = true;
 
-// check if the letter is a valid letter
-function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
-  }
+
 async function init() {
+    // start state
+    let currentRow = 0;
+    let currentGuess = '';
+    let done = false;
+    let isLoading = true;
 
+    
+    // check if the letter is a valid letter
+    function isLetter(letter) {
+        return /^[a-zA-Z]$/.test(letter);
+    }
+
+    // adding letter
+    function addLetter(letter) {
+        // if the length of currentGuess is less than 4 append the letter that has just been input
+        if (currentGuess.length < ANSWER_LENGTH) {
+            currentGuess += letter;
+            // if the lenght of currentGuess it equal to 4 then save the guess
+            // to current var which will substring so... 
+        } else {
+            current = currentGuess.substring(0, currentGuess.length - 1) + letter;
+        }
+        // index -> ex. 1 x 4 + 1 -1 = 2 -> counting on which place you are currebtly
+        letters[currentRow * ANSWER_LENGTH + currentGuess.length - 1].innerText = letter;
+    }
+
+    //TODO: commit() function 
+
+    // display letter in console
     document.addEventListener('keydown', function handleKeyPress (event) {
         const action = event.key;
 
@@ -38,13 +58,5 @@ async function init() {
     });
 }
 
-function handleInput(event) {
-    // check if 
-    if (event.type == "keydown") {
-        console.log(event.key);
-        letters.innerHTML = event.key;
-    }
-    
-}
 init();
 
