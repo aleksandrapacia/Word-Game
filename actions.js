9b5a7b7828ac8eb5f8492347f18464b9d548a04f
Aleksandra Pacia <olusiaop05@gmail.com>
2024-10-16 00:16:11 +0200

    check whether the input is valid and start state

--- actions.js
+++ actions.js
@@ -3,6 +3,19 @@ console.log(letters);
 const loadingDiv = document.querySelector('#loading');
 const validWords = ["code", 'game']
 
+const ANSWER_LENGTH = 4;
+const ROUNDS = 6;
+
+// start state
+let currentRow = 0;
+let currentGuess = '';
+let done = false;
+let isLoading = true;
+
+// check if the letter is a valid letter
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+  }
 async function init() {
 
     document.addEventListener('keydown', function handleKeyPress (event) {
@@ -26,8 +39,10 @@ async function init() {
 }
 
 function handleInput(event) {
+    // check if 
     if (event.type == "keydown") {
         console.log(event.key);
+        letters.innerHTML = event.key;
     }
     
 }
